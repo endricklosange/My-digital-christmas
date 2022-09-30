c478d6449729f76f634337063c6efd320368b6e2
root <root@DESKTOP-HHINTNO.localdomain>
2022-09-30 15:24:09 +0200

    feat(Message+Redirection): Configuration MessageType + Redirection

--- app/src/Form/MessageType.php
+++ app/src/Form/MessageType.php
@@ -2,18 +2,29 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contenu')
-            ->add('receiver')
+            ->add('contenu', TextareaType::class, [
+                'label' => 'Saisie ton message',
+                'required' => true,
+            ])
+            ->add('receiver', EntityType::class, [
+                'label' => 'Qui est l\'heureux élu ?',
+                'required' => true,
+                'class' => User::class
+            ])
         ;
     }
 
